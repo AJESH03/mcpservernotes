af87d2f607ceccf3bb2b4a8b39171f88eeb58f8a
Ajesh <ajesh@Ajeshs-MacBook-Air.local>
2026-02-09 23:56:34 +0530

    Initial commit

---
+++ LocalServer.py
@@ -0,0 +1,39 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("LocalNotes")
+
+@mcp.tool()
+def add_note_to_file(content: str) -> str:
+    """
+    Appends the given content to the user's local notes.
+    Args:
+        content: The text content to append.
+    """
+
+    filename = '/Users/ajesh/Downloads/MCP/MCP_Server_DeeDive_Functionality/notes.txt'
+
+    try:
+        with open(filename, "a", encoding="utf-8") as f:
+            f.write(content + "\n")
+        return f"Content appended to {filename}."
+    except Exception as e:
+        return f"Error appending to file {filename}: {e}"
+    
+
+@mcp.tool()
+def read_notes() -> str:
+    """
+    Reads and returns the contents of the user's local notes.
+    """
+    filename = '/Users/ajesh/Downloads/MCP/MCP_Server_DeeDive_Functionality/notes.txt'
+
+    try:
+        with open(filename, "r", encoding="utf-8") as f:
+            notes = f.read()
+        return notes if notes else "No notes found."
+    except FileNotFoundError:
+        return "No notes file found."
+    except Exception as e:
+        return f"Error reading file {filename}: {e}"
+    
+mcp.run(transport="streamable-http")
